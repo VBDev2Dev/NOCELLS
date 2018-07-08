a4c50a143a1540cddc9325b1f822a4e2d5ebe447
Jay Asbury <vbjay.net@gmail.com>
2018-07-08 18:17:08 -0400

    Add Person data model class

---
+++ NO CELLS/Person.vb
@@ -0,0 +1,22 @@
+﻿Public Class Person
+
+	 Overrides Function ToString() As String
+		  Return $"{FirstName} {LastName} ({ID:B}) is {Age.TotalDays:N0} days old and has {ColorTranslator.ToHtml(HairColor)} hair color."
+
+	 End Function
+
+	 ReadOnly Property Age As TimeSpan
+		  Get
+				Return Now - Birthdate
+
+		  End Get
+	 End Property
+	 Property Birthdate As Date = Now
+	 Property CanDrive As Boolean
+	 Property FirstName As String
+	 Property HairColor As Color = Color.Black
+
+	 Property ID As Guid = Guid.NewGuid
+	 Property LastName As String
+
+End Class
