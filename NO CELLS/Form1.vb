bf3e441b7c743680eb67116a23835434d4adbd2b
Jay Asbury <vbjay.net@gmail.com>
2018-09-18 22:07:56 -0400

    Generate fake data

--- NO CELLS/Form1.vb
+++ NO CELLS/Form1.vb
@@ -14,4 +14,23 @@ Public Class Form1
 		  PersonBindingSource.DataSource = New BindingList(Of Person)(people)
 		  BirthdateDataGridViewCalendarColumn.MaxDate = Now
 	 End Sub
+
+	 Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+		  Static rand As New Random
+		  Dim randString = Function(chars As Integer)
+									  Return New String(Enumerable.Range(0, chars).Select(Function(n) ChrW(rand.Next(65, 101))).ToArray)
+								 End Function
+		  PersonBindingSource.DataSource = Nothing
+		  people.AddRange(Enumerable.Range(0, 50).Select(Function(n) New Person With
+																	 {
+																		 .FirstName = randString(20),
+																		 .LastName = randString(30),
+																		 .CanDrive = rand.Next(-1, 1),
+																		 .HairColor = Color.FromArgb(255, rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256)),
+																		 .Birthdate = Now.AddYears(-18).AddMonths(-4).AddDays(rand.Next(0, 365) * -1)
+																	  })
+							)
+
+		  PersonBindingSource.DataSource = New BindingList(Of Person)(people)
+	 End Sub
 End Class
