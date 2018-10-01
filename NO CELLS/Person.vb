59d358927fe6a6ad202832bc62096613f600335d
Jay Asbury <vbjay.net@gmail.com>
2018-09-30 23:47:18 -0400

    Add INotifyPropertyChanged to Person class

--- NO CELLS/Person.vb
+++ NO CELLS/Person.vb
@@ -1,5 +1,13 @@
-﻿Public Class Person
+﻿Imports System.ComponentModel
 
+Public Class Person
+	 Implements ComponentModel.INotifyPropertyChanged
+
+	 Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+	 Protected Overridable Sub OnPropertyChanged(e As PropertyChangedEventArgs)
+		  RaiseEvent PropertyChanged(Me, e)
+	 End Sub
 	 Overrides Function ToString() As String
 		  Return $"{FirstName} {LastName} ({ID:B}) is {Age.TotalDays:N0} days old and has {ColorTranslator.ToHtml(HairColor)} hair color. They are a {If(CanDrive, "Driver", "Passenger")}."
 
@@ -11,12 +19,88 @@
 
 		  End Get
 	 End Property
-	 Property Birthdate As Date = Now
+
+	 Dim _birthdate As Date = Now
+	 Property Birthdate As Date
+		  Get
+				Return _birthdate
+		  End Get
+		  Set
+				If _birthdate = Value Then
+					 Return
+				End If
+				_birthdate = Value
+				OnPropertyChanged(New PropertyChangedEventArgs(NameOf(Birthdate)))
+		  End Set
+	 End Property
+
+	 Dim _canDrive As Boolean
 	 Property CanDrive As Boolean
+		  Get
+				Return _canDrive
+		  End Get
+		  Set
+				If _canDrive = Value Then
+					 Return
+				End If
+				_canDrive = Value
+				OnPropertyChanged(New PropertyChangedEventArgs(NameOf(CanDrive)))
+		  End Set
+	 End Property
+
+	 Dim _firstName As String
 	 Property FirstName As String
-	 Property HairColor As Color = Color.Black
+		  Get
+				Return _firstName
+		  End Get
+		  Set
+				If _firstName Is Value Then
+					 Return
+				End If
+				_firstName = Value
+				OnPropertyChanged(New PropertyChangedEventArgs(NameOf(FirstName)))
+		  End Set
+	 End Property
 
-	 Property ID As Guid = Guid.NewGuid
+	 Dim _hairColor As Color = Color.Black
+	 Property HairColor As Color
+		  Get
+				Return _hairColor
+		  End Get
+		  Set
+				If _hairColor = Value Then
+					 Return
+				End If
+				_hairColor = Value
+				OnPropertyChanged(New PropertyChangedEventArgs(NameOf(HairColor)))
+		  End Set
+	 End Property
+
+	 Dim _iD As Guid = Guid.NewGuid
+	 Property ID As Guid
+		  Get
+				Return _iD
+		  End Get
+		  Set
+				If _iD = Value Then
+					 Return
+				End If
+				_iD = Value
+				OnPropertyChanged(New PropertyChangedEventArgs(NameOf(ID)))
+		  End Set
+	 End Property
+
+	 Dim _lastName As String
 	 Property LastName As String
-
+		  Get
+				Return _lastName
+		  End Get
+		  Set
+				If _lastName Is Value Then
+					 Return
+				End If
+				_lastName = Value
+				OnPropertyChanged(New PropertyChangedEventArgs(NameOf(LastName)))
+		  End Set
+	 End Property
 End Class
